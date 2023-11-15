ba0282fff30075c748d928245e0f5c56685baa3a
Pranav Madhu <pranavmadhu73@gmail.com>
2023-11-15 21:50:14 +0530

    removed the unnecessary code from recommendations.py , added requirements.txt, updated gitignore

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
+from recommendations import 
+    get_index_from_name,
+    get_id_from_partial_name,
+    find_similar_books_by_author,
+    find_similar_books_by_publisher,
+    print_similar_books,
+    recommend_books_by_average_rating,
 
+get
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello():
-    return render_template('index.html')
+    return render_template("index.html")
+
+
+
