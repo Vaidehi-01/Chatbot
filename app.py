ad3f4d4a505764dc1d9ee827d7bd8f88ffe8f9ea
Pranav Madhu <pranavmadhu73@gmail.com>
2023-11-30 19:32:13 +0530

    fix: route manipulation issue when query contains /

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 from recommendations import (
     find_similar_books_by_author,
     find_similar_books_by_publisher,
@@ -15,27 +15,26 @@ def hello():
     return render_template("index.html")
 
 
-@app.route("/recommendations/<case>/<query>")
-def recommendations(case, query):
-    case = str(case)
-    query = str(query)
-    case = case.strip()
-    query = query.strip()
-    print(query)
-    if case == "1":
-        result = find_similar_books_by_author(query)
-        return jsonify(result)
-    elif case == "2":
-        result = find_similar_books_by_publisher(query)
-        return jsonify(result)
-    elif case == "3":
-        result = get_id_from_partial_name(query)
-        return jsonify(result)
-    elif case == "4":
-        result = recommend_books_by_average_rating(num_recommendations=int(query))
-        return jsonify(result)
-    elif case == "5":
-        result = get_index_from_name(query)
-        return jsonify(result)
-    else:
-        return jsonify("Invalid Input or invalid case number")
+@app.route("/recommendations/<case>", methods=["POST"])
+def recommendations(case):
+    if request.method == "POST":
+        query = request.json.get("query")
+        case = str(case)
+        case = case.strip()
+        if case == "1":
+            result = find_similar_books_by_author(query)
+            return jsonify(result)
+        elif case == "2":
+            result = find_similar_books_by_publisher(query)
+            return jsonify(result)
+        elif case == "3":
+            result = get_id_from_partial_name(query)
+            return jsonify(result)
+        elif case == "4":
+            result = recommend_books_by_average_rating(num_recommendations=int(query))
+            return jsonify(result)
+        elif case == "5":
+            result = get_index_from_name(query)
+            return jsonify(result)
+        else:
+            return jsonify("Invalid Input or invalid case number")
