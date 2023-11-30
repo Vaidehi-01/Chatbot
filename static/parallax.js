4cb6b758359774efe4815489ada76e84b05f48d4
Vaidehi Deshpande <vaidehi010301@gmail.com>
2023-11-30 19:41:20 +0530

    Co-authored-by: Pranav Madhu <pranavmadhu01@users.noreply.github.com>

--- static/parallax.js
+++ static/parallax.js
@@ -1,36 +1,36 @@
 const parallaxArray = [
   {
-    title: "deserunt dolores molestiae 1",
+    title: "Book Buddy",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati beatae perferendis deserunt dolores molestiae hic dolorum maxime numquam, ad rerum quas optio quis ut",
+      "Presenting Book Buddy. Our Book Recommendation Chatbot:)",
     image:
       "https://images.unsplash.com/photo-1547856194-dd2e1bd5d25e?auto=format&fit=crop&q=80&w=1932&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
-    title: "deserunt dolores molestiae 1",
+    title: "Joseph Addison",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati beatae perferendis deserunt dolores molestiae hic dolorum maxime numquam, ad rerum quas optio quis ut",
+      "Reading is to the mind what exercise is to the body!!",
     image:
       "https://images.unsplash.com/photo-1577985051167-0d49eec21977?auto=format&fit=crop&q=80&w=2089&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
-    title: "deserunt dolores molestiae 1",
+    title: "Neil Gaiman",
     description:
-      " Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati beatae perferendis deserunt dolores molestiae hic dolorum maxime numquam, ad rerum quas optio quis ut",
+      "A book is a dream that you hold in your hand!! ",
     image:
       "https://images.unsplash.com/photo-1481627834876-b7833e8f5570?auto=format&fit=crop&q=80&w=2128&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
-    title: "deserunt dolores molestiae 1",
+    title: "Jorge Luis Borges",
     description:
-      " Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati beatae perferendis deserunt dolores molestiae hic dolorum maxime numquam, ad rerum quas optio quis ut",
+      "I have always imagined that Paradise will be a kind of library!!",
     image:
       "https://images.unsplash.com/photo-1533327325824-76bc4e62d560?auto=format&fit=crop&q=80&w=2069&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
-    title: "deserunt dolores molestiae 1",
+    title: "Charles William Eliot",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati beatae perferendis deserunt dolores molestiae hic dolorum maxime numquam, ad rerum quas optio quis ut",
+      "Books are the quietest and most constant of friends; they are the most accessible and wisest of counselors, and the most patient of teachers!! ",
     image:
       "https://images.unsplash.com/photo-1414124488080-0188dcbb8834?auto=format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
